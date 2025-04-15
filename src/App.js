0332dd91a01ecdd8fdba0ac6d3aebd34cfacb852
alvin1284 <alvinwaweru43@gmail.com>
2025-04-15 15:46:22 +0300

    feat: initialize React app with essential files and components
    - Added package.json with dependencies and scripts for React app.
    - Created public assets including favicon, index.html, and manifest.json.
    - Implemented App component with movie search functionality using OMDB API.
    - Added MovieCard component to display individual movie details.
    - Styled the application with App.css for a cohesive look.
    - Included search icon SVG for the search functionality.

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import MovieCard from "./MovieCard";
+
+
+import "./App.css";
+import searchIcon from "./search.svg";
+// import React{ useEffect } from "react";
+
+// 8a9580cd
+
+const API_URL = 'http://www.omdbapi.com?apikey=8a9580cd'
+
+// const movie1 = {
+//     "Title": "The Shawshank Redemption",
+//     "Year": "1994",
+//     "imdbID": "tt0111161",
+//     "Type": "movie",
+//     "Poster": "https://m.media-amazon.com/images/M/MV5BMDAyY2FhYjctNDc5OS00MDNlLThiMGUtY2UxYWVkNGY2ZjljXkEyXkFqcGc@._V1_SX300.jpg"
+
+
+// }
+
+const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('The Shawshank Redemption');
+    }, [])
+
+    return (
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input
+                    placeholder="Search for a movie"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)} />
+                <img
+                    src={searchIcon}
+                    alt="search"
+                    onClick={() => searchMovies(searchTerm)} />
+            </div>
+
+            {
+                movies?.length > 0 ? (
+                    <div className="container">
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie} />
+                        ))}
+                    </div>
+
+                ) :
+                    (
+                        <div className="empty">
+                            <h2>No movies found</h2>
+                        </div>
+                    )
+            }
+
+        </div>
+    );
+}
+export default App;
